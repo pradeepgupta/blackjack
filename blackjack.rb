c00b87beb14e4f55a27e05836a365db4d6417775
pradeepgupta <pradeepc2c@yahoo.com>
2013-04-06 00:05:31 +0530

    changed arrays

--- blackjack.rb
+++ blackjack.rb
@@ -4,5 +4,19 @@ hearts = [["Jacks","11"],["Queens","12"], ["Kings", "13"],[" Aces","11","1"],["2
 spades = [["Jacks","11"],["Queens","12"], ["Kings", "13"],[" Aces","11","1"],["2","3","4","5","6","7","8","9","10"]]
 
 cards = [clubs,diamonds,hearts,spades]
+random = rand(cards.length)
 
-cards.each 
+puts "What is Player name?"
+name1 = gets.chomp
+
+puts "#{name1} Please Hit to draw your two cards"
+puts "Press Hit to play the game"
+puts "Press quit to quit the game"
+
+response = gets.chomp.downcase
+
+while response == "hit"
+
+puts "Your Cards:" + random
+
+end
