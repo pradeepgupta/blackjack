dcd8fe1977566d41525ec9e97e9a457747f1455b
pradeepgupta <pradeepc2c@yahoo.com>
2013-04-11 16:56:20 +0530

    Did Refactoring

--- blackjack.rb
+++ blackjack.rb
@@ -1,6 +1,7 @@
-# Interactive command line blackjack game
+# BlaclJack Procedural Game Written By Pradeep Gupta
+# Version 1.0.0
 
-
+# Method to calculate Total
 def calculate_total(cards) 
   # [['H', '3'], ['S', 'Q'], ... ]
   arr = cards.map{|e| e[1] }
@@ -24,6 +25,7 @@ def calculate_total(cards)
   total
 end
 
+# Start Of Game
 puts "Welcome to Blackjack!"
 
 suits = ['H', 'D', 'S', 'C']
@@ -42,97 +44,109 @@ dealercards << deck.pop
 mycards << deck.pop
 dealercards << deck.pop
 
-@dealertotal = calculate_total(dealercards)
-@mytotal = calculate_total(mycards)
+dealertotal = calculate_total(dealercards)
+mytotal = calculate_total(mycards)
 
 # Show Cards
 
-puts "Dealer has: #{dealercards[0]} and #{dealercards[1]}, for a total of #{@dealertotal}"
-puts "You have: #{mycards[0]} and #{mycards[1]}, for a total of: #{@mytotal}"
+puts "Dealer has: #{dealercards[0]} and #{dealercards[1]}, for a total of #{dealertotal}"
+puts "You have: #{mycards[0]} and #{mycards[1]}, for a total of: #{mytotal}"
 
-if @mytotal == 21
-	puts "you win"
+# Check for existing points 
+
+if mytotal == 21
+  puts "you win"
+  exit
 end
 
-if @dealertotal == 21
+if dealertotal == 21
 	puts " Dealer win"
+  exit
 end
 
-until @mytotal == 21 || @dealertotal == 21  # For My Turn
+# Player Turn
+while mytotal < 21
 
-   if @mytotal > 21
-		puts "You lost the game"
-		break
+  puts "What would you like to do? 1) hit 2) stay"
+  hit_or_stay = gets.chomp.downcase
 
-	 elsif @mytotal == 21
-		puts "You win the game"
-		break
-
-	 elsif @mytotal < 21
-		                    puts "What would you like to do? 1) hit 2) stay"
-                        hit_or_stay = gets.chomp
                  
-                 if hit_or_stay == "hit"
-                    mynewcards = []
-        	          mynewcards << deck.pop
-        	          @mytotal += calculate_total(mynewcards)
-                    puts "#{@mytotal}"
-        	 
-        	            if @mytotal == 21
-        	 	             puts " you win"
-        	 	             break
-        	 
-        	           elsif @mytotal > 21
-        	 	            puts " you loose"
-        	 	            break
-                      else @mytotal < 21
-                        puts
-                      end
-        	 
-        	        else 
-                    puts "your points are: #{@mytotal}"
-                    break
-                end
-                
-    else
-        puts ""
-        break
-	 end
-end
-@mytotal
+  if hit_or_stay == "hit"
+    
+    new_card = deck.pop
+    puts "Dealing card to player: #{new_card}"
+    mycards << new_card
+    mytotal = calculate_total(mycards)
+    puts "# My news card is #{mycards} and New total is: #{mytotal}"
+           
+    if mytotal == 21
+      puts " you got BlackJack and Dealer lost the game!"
+      exit
+           
+    elsif mytotal > 21
+      puts " you loose the game and dealer win the game!"
+      exit
 
-until (@mytotal == 21 || @dealertotal == 21)  # for dealer
+    else mytotal < 21
+      next
+    end
+           
+  else hit_or_stay == "stay"
+    puts "your points are: #{mytotal} and now dealer's turn.."
+    break
+  end
+end
+
+# Dealer Turn
+while dealertotal < 17
  
-  puts "Now Dealers turn"
-  puts " Dealer you should hit until you got 17, Now Press Hit"
-        hit_or_stay = gets.chomp.downcase
+  puts "Now Dealers turn.."
+  puts "Press hit to proceed the game.."
+  hit_or_stay = gets.chomp.downcase
+  puts " Dealer press hit "
 
-        if hit_or_stay == "hit"
+  if hit_or_stay == "hit"
           
+    new_card = deck.pop
+    puts "Dealing new card for dealer: #{new_card}"
+    dealercards << new_card
+    dealertotal = calculate_total(dealercards)
             
-            newdealercards = []
-            newdealercards << deck.pop
-            @dealertotal += calculate_total(newdealercards)
+    puts "Dealers new card is #{dealercards} and new points are : #{dealertotal}"
+
+    if dealertotal == 21
+      puts "Sorry, dealer hit blackjack. You lose."
+      exit
+    elsif dealertotal > 21
+      puts "Congratulations, dealer busted! You win!"
+      exit
+    end
             
-            puts "Dealers points are : #{@dealertotal}"
-            comparison = @dealertotal <=> @mytotal
-              
-              if comparison == -1.to_i
-                puts "You win"
-                
-                elsif comparison == 0.to_i
-                   puts " Tie"
-                
-              else comparison == 1.to_i
-                 if @dealertotal > 21
-                   puts " Dealer bust and you win"
-                 else
-                  puts "Dealer win"
-                end
-              end
-            
-        else
-            puts " please press correct hit word"
-        end
-     break 
+  else
+    puts " please press correct hit word"
+    next
+  end
 end
+
+# Comparison of both dealer and players points
+comparison = dealertotal <=> mytotal
+              
+if comparison == -1.to_i
+  puts "You win"
+  exit
+                
+elsif comparison == 0.to_i
+  puts " Tie"
+  exit
+                
+else comparison == 1.to_i
+  if dealertotal > 21
+    puts " Dealer bust and you win"
+    exit
+  else
+    puts "Dealer win and you bust"
+    exit
+  end
+end
+
+exit
